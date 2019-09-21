22e793316d90e88e76ac5ebad24d098b8dd03061
Danilo Teodoro <danilo.santost@gmail.com>
2019-09-21 08:33:39 +0200

    Delete old files, add new react app

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Home: React.FC = () => {
+  return (
+    <div className="root">
+      no content yet
+    </div>
+  );
+}
+
+export default Home;
